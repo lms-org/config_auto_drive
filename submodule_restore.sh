b8c6948a4fec87d5393965d848b7ea9010d566fa
Philipp <philipp.schmette@tum.de>
2015-04-29 14:50:18 +0200

    added restore script

---
+++ submodule_restore.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#set some values
+set -e
+
+#Check params
+force=false
+initGit=false
+while getopts ":cp" opt; do :
+    case $opt in
+#TODO f doesn't work?!
+      c )
+       force=true
+      i )
+       initGit=true
+    esac
+done
+########################################
+################DO STUFF################
+########################################
+
+#(re)-init git if wanted
+if [ "$initGit" = true]; then
+    rm -rf .git
+    git init
+fi
+
+#TODO why is a tempfile is needed?
+git config -f .gitmodules --get-regexp '^submodule\..*\.path$' > tempfile
+#read 3 lines etc.
+while read -u 3 path_key path
+do
+    url_key=$(echo $path_key | sed 's/\.path/.url/')
+    url=$(git config -f .gitmodules --get "$url_key")
+#TODO Check if directory exists, if not ignore force
+    if [ "$force" = false ];then
+            read -p "Are you sure you want to delete $path and re-initialize as a new submodule? " yn
+    case $yn in
+        [Nn]* ) continue;;
+        * ) echo "Please answer yes or no."
+        continue;;
+    esac
+    fi
+    #create submodule
+    rm -rf $path
+    git submodule add $url $path
+    echo "$path has been initialized"
+
+done 3<tempfile
+
+rm tempfile
