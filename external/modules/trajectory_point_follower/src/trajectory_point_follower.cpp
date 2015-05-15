57dc69b153b8a3904d7ee433e1dcbd36c78b305a
Philipp <philipp.schmette@tum.de>
2015-05-15 18:58:07 +0200

    some pictures

--- external/modules/trajectory_point_follower/src/trajectory_point_follower.cpp
+++ external/modules/trajectory_point_follower/src/trajectory_point_follower.cpp
@@ -13,12 +13,12 @@ bool TrajectoryLineFollower::deinitialize() {
 
 bool TrajectoryLineFollower::cycle() {
 
-   /* std::pair<float,float> steering = TobisRegler_Simpel((*trajectoryPoint)[0], (*trajectoryPoint)[1],
-            atan2((*trajectoryPoint)[3], (*trajectoryPoint)[2]), 0.5);*/
+    std::pair<float,float> steering = TobisRegler_Simpel((*trajectoryPoint)[0], (*trajectoryPoint)[1],
+            atan2((*trajectoryPoint)[3], (*trajectoryPoint)[2]), 0.5);
 
 
-    std::pair<float,float> steering = smartRegler((*trajectoryPoint)[0], (*trajectoryPoint)[1],
-            atan2((*trajectoryPoint)[3], (*trajectoryPoint)[2]));
+    //std::pair<float,float> steering = smartRegler((*trajectoryPoint)[0], (*trajectoryPoint)[1],
+      //      atan2((*trajectoryPoint)[3], (*trajectoryPoint)[2]));
 
     controlData->vel_mode = Comm::SensorBoard::ControlData::MODE_VELOCITY;
     controlData->steering_front = steering.first;// * 180. / M_PI;
