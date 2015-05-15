ed0cf2891bf0435d8511d2f0770c57a82bdc4537
Hans Kirchner <hans-kirchner@gmx.de>
2015-05-15 13:44:58 +0200

    Add Tobis Regler

--- external/modules/trajectory_point_follower/include/trajectory_point_follower.h
+++ external/modules/trajectory_point_follower/include/trajectory_point_follower.h
@@ -5,6 +5,9 @@
 #include "lms/datamanager.h"
 #include "lms/imaging/find/image_hint.h"
 #include "image_objects/environment.h"
+#include "lms/math/vertex.h"
+
+#include "comm/senseboard.h"
 
 class TrajectoryLineFollower : public lms::Module {
 public:
@@ -12,6 +15,10 @@ public:
     bool deinitialize() override;
     bool cycle() override;
 private:
+    std::pair<float, float> TobisRegler(double x_s_n, double y_s_n, double phi_s, double v);
+
+    Comm::SensorBoard::ControlData *controlData;
+    const lms::math::vertex<4,float> *trajectoryPoint;
 };
 
 #endif /* IMAGE_HINT_TRANSFORMER_H */
