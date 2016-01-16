b32aadaf224acbd68bbc91a3d84dc5d5fec9c1f6
phibedy <philipp.schmette@gmx.de>
2016-01-16 18:57:07 +0100

    added crossing and startline serialization

--- external/modules/socket_datatype_definer/src/socket_datatype_definer.cpp
+++ external/modules/socket_datatype_definer/src/socket_datatype_definer.cpp
@@ -1,13 +1,14 @@
 #include "socket_datatype_definer.h"
 #include "lms/math/polyline.h"
 #include "street_environment/trajectory.h"
+#include "street_environment/street_environment.h"
 #include "sensor_utils/car.h"
 
 bool SocketDatatypeDefiner::initialize() {
     readChannel<street_environment::Trajectory>("TRAJECTORY_LINE"); //TODO change it to trajectory
     readChannel<street_environment::TrajectoryPoint>("TRAJECTORY_POINT");
     readChannel<sensor_utils::Car>("CAR");
-    //datamanager()->readChannel<std::pair<lms::math::vertex2f,lms::math::vertex2f>(this,"TRAJECTORY_POINT");
+    readChannel<street_environment::EnvironmentObjects>("ENVIRONMENT_OBSTACLE");
     return true;
 }
 
