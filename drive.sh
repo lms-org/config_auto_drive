72a9e8da45b2a8280e19bc7dc6c58da20c015699
lkskstlr <lkskstlr@gmail.com>
2016-02-05 12:45:10 +0100

    added new observation model based on line fitting

--- drive.sh
+++ drive.sh
@@ -1,2 +1,2 @@
-./build/lms -c mycam --flags files,sendData,sdl --enable-load /home/phibedy/CC2016/box_g  "$@"
-#./build/bin/lms -c mycam --flags files,sendData,sdl --enable-load /Users/lukas/phoenix_lms/daten/box  "$@"
+#./build/lms -c mycam --flags files,sendData,sdl --enable-load /home/phibedy/CC2016/box_g  "$@"
+./build/bin/lms -c mycam --flags files,sendData,sdl --enable-load /Users/lukas/phoenix_lms/daten/2016-01-25T17-11-30Z-schnelle_runde  "$@"
