fb2549ee1d5fc2b798e64bcaeea9cb494f96cfcd
phibedy <philipp.schmette@gmx.de>
2016-09-29 18:00:01 +0200

    added urg laser scanner

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class AutoDriveConan(ConanFile):
     version = "1.0"
     settings = None
     exports = "*"
-    requires = "lms_camera_importer/1.0@lms/stable","lms_sdl_service/1.0@lms/stable","lms_sdl_image_renderer/1.0@lms/stable"
+    requires = "lms_camera_importer/1.0@lms/stable","lms_sdl_service/1.0@lms/stable","lms_sdl_image_renderer/1.0@lms/stable","lms_urg_laser_scanner/1.0@lms/stable"
 
     def imports(self):
         self.copy("*.so",dst="bin")
