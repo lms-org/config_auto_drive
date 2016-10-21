5d382d9ffcffbb8a77bdf91b64402e372cde553a
phibedy <philipp.schmette@gmx.de>
2016-10-21 18:21:58 +0200

    updated libs

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class AutoDriveConan(ConanFile):
     version = "1.0"
     settings = None
     exports = "README.md","start.sh"
-    requires = "lms/2.0@lms/stable","cereal/1.2-0@lms/stable","gtest/1.8.0@lms/stable" #"lms_camera_importer/1.0@lms/stable","lms_sdl_service/1.0@lms/stable","lms_sdl_image_renderer/1.0@lms/stable","lms_urg_laser_scanner/1.0@lms/stable","lms_image_converter/1.0@lms/stable","phoenix_service/1.0@lms/stable","image_hint_generator/1.0@lms/stable","image_hint_worker/1.0@lms/stable","image_hint_transformer/1.0@lms/stable","image_hint_worker/1.0@lms/stable","lap_time/1.0@lms/stable","socket_datatype_definer/1.0@lms/stable","lms_ximea_importer/1.0@lms/stable","lms_pause_runtime/1.0@lms/stable"#,"local_course_service/1.0@lms/stable"
+    requires = "lms/2.0@lms/stable","cereal/1.2-0@lms/stable","gtest/1.8.0@lms/stable","SDL2/2.0.4@lasote/stable","urg_network/1.2.0@lms/stable","Eigen3/3.2.8@bilke/stable"
 
     def imports(self):
         self.copy("*.so",dst=".")
