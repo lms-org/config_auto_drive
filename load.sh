33b5ce8be12cc6fc4286cb39052058433878d512
phibedy <philipp.schmette@gmx.de>
2016-11-15 20:40:11 +0100

    .

--- load.sh
+++ load.sh
@@ -1,2 +1,2 @@
- ./build/bin/lms run ./configs/mycam.xml --log info --debug -l ./build/lib -s --enable-load /home/phibedy/Desktop/logs/langsame_runde/lmslogs-test -f load
+ ./build/bin/lms run ./configs/mycam.xml --log warn --debug -l ./build/lib -s --enable-load /home/phibedy/Desktop/logs/langsame_runde/lmslogs-test -f load
 
