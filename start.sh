8c3b5fd93ab1cafe2ea737e320bd8767c7cc4ab5
Your Name <you@example.com>
2016-11-17 18:04:06 +0100

    :

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
- ./build/bin/lms run ./configs/new_car.xml -l ./build/lib --log warn -f norender -f sendData
+ ./build/bin/lms run ./configs/new_car.xml -l ./build/lib --log warn --enable-save slow_round -f norender -f sendData -f save-car
