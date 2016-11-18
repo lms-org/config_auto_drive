3851d7d85149f2d1181dd68ea32c3a233d26b61d
phibedy <philipp.schmette@gmx.de>
2016-11-18 23:54:53 +0100

    fixed qt-service + added fileName to channel_logger/loader

--- external/services/qt_gui_service/src/qt_gui_service.cpp
+++ external/services/qt_gui_service/src/qt_gui_service.cpp
@@ -5,8 +5,15 @@ namespace qt_gui_service {
 
 bool QtGuiService::init() {
     //std::vector<std::string> qArgs = config().getArray<std::string>("QArgs");
-    int argc = 0;//qArgs.size();
-    char** argv = nullptr;
+    /*
+
+    Warning: The data referred to by argc and argv must stay valid for the entire lifetime of the QApplication object.
+ In addition, argc must be greater than zero and argv must contain at least one valid character string.
+
+     *
+     */
+    argc = 0;//qArgs.size();
+    argv = nullptr;
     if(argc > 0){
         //TODO argv;
     }
--- load.sh
+++ load.sh
@@ -1,2 +1,2 @@
- ./build/bin/lms run ./configs/mycam.xml --log warn --debug -l ./build/lib -s --enable-load /home/phibedy/Desktop/logs/langsame_runde/lmslogs-test -f load
+ ./build/bin/lms run ./configs/mycam.xml --log warn --debug -l ./build/lib -s --enable-load /home/phibedy/Desktop/logs/fast_round -f load-car
 
