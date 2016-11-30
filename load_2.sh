a7c3c923877dcdb1732015f6e1c1823d0f030de9
Arthur Mathies <arthur.mathies@googlemail.com>
2016-11-30 19:44:48 +0100

    Ready to drive (hopefully)

---
+++ load_2.sh
@@ -0,0 +1,1 @@
+./build/bin/lms run ./configs/start_car.xml --log warn --debug -l ./build/lib -s --enable-load /home/arthur/Desktop/ -f load -f render_sdl -f load_pgm -f start_lidar
---
+++ start_2.sh
@@ -0,0 +1,1 @@
+./build/bin/lms run ./configs/start_car.xml -l ./build/lib --log warn -f start_lidar
