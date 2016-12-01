3920f55b6e0b68ee79d713cf5263b714f9181880
phibedy <philipp.schmette@gmx.de>
2016-12-01 14:14:26 +0100

    improved configs

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
- ./build/bin/lms run ./configs/new_car.xml -l ./build/lib --log warn --enable-save fast_4_round -s -f norender -f sendData -f save-car
+./build/bin/lms run ./configs/start_car.xml --debug -s -l ./build/lib --log debug -f send -f render_sdl -f start_lidar
--- start_2.sh
+++ start_2.sh
@@ -1,1 +1,1 @@
-./build/bin/lms run ./configs/start_car.xml -l ./build/lib --log warn -f start_lidar
+./build/bin/lms run ./configs/start_car.xml -l ./build/lib --log warn -f start_lidar -f send
