caee9a4c8471077986f8cd82abf69aefcc3b0998
phibedy <philipp.schmette@gmx.de>
2016-12-20 11:50:56 +0100

    merge

--- load_2.sh
+++ load_2.sh
@@ -1,1 +1,1 @@
-./build/bin/lms run ./configs/start_car.xml --log warn --debug -l ./build/lib -s --enable-load /home/arthur/Desktop/ -f load -f render_sdl -f load_pgm -f start_lidar
+./build/bin/lms run ./configs/start_car.xml --log warn --debug -l ./build/lib -s --enable-load /home/phibedy/Desktop/logs/langsame_runde/lmslogs-test -f load -f render_sdl -f load_pgm
