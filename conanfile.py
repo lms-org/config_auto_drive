36f0ac6c8c4baf5392d23531fc858fe638da5013
phibedy <philipp.schmette@gmx.de>
2016-12-20 15:30:32 +0100

    using conan opencv

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,8 @@ class AutoDriveConan(ConanFile):
     version = "1.0"
     settings = None
     exports = "README.md","start.sh"
-    requires = "lms/2.0@lms/stable","cereal/1.2-0@lms/stable","gtest/1.8.0@lms/stable","urg_network/1.2.0@lms/stable","Eigen3/3.2.8@bilke/stable","kalman/1.0@lms/stable"#,"OpenCV/2.4.13@lms/stable" #"SDL2/2.0.4@lasote/stable"
+    requires = "lms/2.0@lms/stable","cereal/1.2-0@lms/stable","gtest/1.8.0@lms/stable","urg_network/1.2.0@lms/stable","Eigen3/3.2.8@bilke/stable","kalman/1.0@lms/stable","OpenCV/2.4.13@lms/stable" #"SDL2/2.0.4@lasote/stable"
+    generators = "cmake"
 
     def imports(self):
         self.copy("*.so",dst=".")
