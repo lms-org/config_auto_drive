4c5f29a6660a1e4fdfbd4ecd4e657daf18eb3224
phibedy <philipp.schmette@gmx.de>
2017-01-23 13:32:47 +0100

    added missing proto libs *.so*,*.a* files

--- conanfile.py
+++ conanfile.py
@@ -5,12 +5,12 @@ class AutoDriveConan(ConanFile):
     version = "1.0"
     settings = None
     exports = "README.md","start.sh"
-    requires = "zlib/1.2.9@lasote/stable","lms/2.0@lms/stable","cereal/1.2-0@lms/stable","gtest/1.8.0@lms/stable","urg_network/1.2.0@lms/stable","Eigen3/3.2.8@bilke/stable","kalman/1.0@lms/stable","OpenCV/2.4.13@lms/stable" #"SDL2/2.0.4@lasote/stable"
+    requires = "lms/2.0@lms/stable","cereal/1.2-0@lms/stable","gtest/1.8.0@lms/stable","urg_network/1.2.0@lms/stable","Eigen3/3.2.8@bilke/stable","kalman/1.0@lms/stable","OpenCV/2.4.13@lms/stable" #"SDL2/2.0.4@lasote/stable"
     generators = "cmake"
 
     def imports(self):
-        self.copy("*.so",dst=".")
-        self.copy("*.a",dst=".")
+        self.copy("*.so*",dst=".")
+        self.copy("*.a*",dst=".")
         self.copy("*.dylib",dst=".")
         self.copy("bin/*",dst=".")
         self.copy("include/*",dst="")
