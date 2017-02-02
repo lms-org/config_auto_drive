367f3f34589a062153ee09ba8287159854cb88b1
Your Name <you@example.com>
2017-02-02 19:59:32 +0100

    .

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./build/bin/lms run ./configs/start_car.xml -l ./build/lib --log warn -f start_lidar -f send
+./build/bin/lms run ./configs/start_car.xml -s -l ./build/lib --log debug --debug -f start_lidar -f send
