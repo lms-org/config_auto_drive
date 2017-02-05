4a564a6c8fa0f4d7fb8ca8ddb7a1f500c4e8adca
phibedy <philipp.schmette@gmx.de>
2017-02-05 23:44:26 +0100

    configs

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 export LD_LIBRARY_PATH="/home/phoenix/deploy_auto_drive-phibedy/build/lib"
-./build/bin/lms run ./configs/start_car.xml -s -l ./build/lib --log debug --debug -f start_lidar -f send
+./build/bin/lms run ./configs/start_car.xml -j 2 -s -l ./build/lib --log error -f start_lidar -f send
