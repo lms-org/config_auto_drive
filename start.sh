daae86dd5930133b2580c3f2116111aae764c257
Your Name <you@example.com>
2018-02-02 16:17:02 +0100

    changed library path

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-export LD_LIBRARY_PATH="/home/phoenix/deploy_auto_drive-phibedy/build/lib"
+export LD_LIBRARY_PATH="/home/phoenix/dev/config_auto_drive/build/lib"
 ./build/bin/lms run ./configs/start_car.xml -s -j 2 -l ./build/lib --log error -f start_lidar -f send
+
