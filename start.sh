202e0ffd5df25e79a7aeb71d1bd13ebd66bd188e
Your Name <you@example.com>
2018-02-10 16:30:28 +0100

    higher processer priority

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH="/home/phoenix/dev/config_auto_drive/build/lib"
-./build/bin/lms run ./configs/start_car.xml -s -j 2 -l ./build/lib --log error -f start_lidar
+nice -n -15 ./build/bin/lms run ./configs/start_car.xml -s -j 2 -l ./build/lib --log error -f start_lidar
 
