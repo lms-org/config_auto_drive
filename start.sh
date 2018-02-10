8a0387786be12827d9b2760b291d8cc8a44a4bbe
Your Name <you@example.com>
2018-02-10 10:41:30 +0100

    disable send in default start file

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH="/home/phoenix/dev/config_auto_drive/build/lib"
-./build/bin/lms run ./configs/start_car.xml -s -j 2 -l ./build/lib --log error -f start_lidar -f send
+./build/bin/lms run ./configs/start_car.xml -s -j 2 -l ./build/lib --log error -f start_lidar
 
